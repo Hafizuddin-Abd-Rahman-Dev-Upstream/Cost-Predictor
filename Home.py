21de0a1ef5041f308df278b9dd8dd5de7f94ab2b
apizrahman24 <hafizuddin.tab@gmail.com>
2025-07-22 12:22:03 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Set page config
 st.set_page_config(
-    page_title="Welcome | Cost Prediction RT2025",
+    page_title="Welcome to CE AI Tools Homepage",
     page_icon="💲",
     layout="centered",
     initial_sidebar_state="collapsed"
