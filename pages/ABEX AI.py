f3d11d2ba057584ec0dd5596944e03c8228fdc26
apizrahman24 <hafizuddin.tab@gmail.com>
2025-07-24 14:56:36 +0800

    Update ABEX AI.py

--- pages/ABEX AI.py
+++ pages/ABEX AI.py
@@ -369,8 +369,8 @@ def main():
     st.markdown("")
     
     col_pd1, col_pd2 = st.columns(2)
-    predev_percentage = col_pd1.number_input("Pre-Development (%)", min_value=0.0, max_value=100.0, value=0.0)
-    owners_percentage = col_pd2.number_input("Owner's Cost (%)", min_value=0.0, max_value=100.0, value=0.0)
+    predev_percentage = col_pd1.number_input("Enter Pre-Development (%)", min_value=0.0, max_value=100.0, value=0.0)
+    owners_percentage = col_pd2.number_input("Enter Owner's Cost (%)", min_value=0.0, max_value=100.0, value=0.0)
     
     # Cost Contingency and Escalation & Inflation Input Section (in one row)
     st.subheader("⚠️ Cost Contingency & 📈 Escalation & Inflation Input")
@@ -378,11 +378,11 @@ def main():
     
     with col_cont1:
         st.markdown("")
-        contingency_percentage = st.number_input("Cost Contingency (%)", min_value=0.0, max_value=100.0, value=0.0)
+        contingency_percentage = st.number_input("Enter Cost Contingency (%)", min_value=0.0, max_value=100.0, value=0.0)
     
     with col_cont2:
         st.markdown("")
-        escalation_percentage = st.number_input("Escalation & Inflation (%)", min_value=0.0, max_value=100.0, value=0.0)
+        escalation_percentage = st.number_input("Enter Escalation & Inflation (%)", min_value=0.0, max_value=100.0, value=0.0)
 
     st.markdown("**High-Level Escalation and Inflation rate is based on compounded percentage for the entire project development.*")
     
