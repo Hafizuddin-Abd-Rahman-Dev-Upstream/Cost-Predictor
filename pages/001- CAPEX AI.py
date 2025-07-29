6ea89ece62d645ad926b6133b08002e5730a67ff
apizrahman24 <hafizuddin.tab@gmail.com>
2025-07-29 12:08:21 +0800

    Update 001- CAPEX AI.py

--- pages/001- CAPEX AI.py
+++ pages/001- CAPEX AI.py
@@ -136,6 +136,7 @@ def main():
             st.sidebar.success("All predictions compiled successfully!")
         else:
             st.sidebar.warning("No predictions to download.")
+    ---
     if st.sidebar.button("🔄 Refresh System"):
         list_csvs_from_github.clear()
     st.sidebar.subheader("📁 Choose Data Source")
