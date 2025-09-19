1c83bea9ec8562ec05c3344d2abb72d259c15f49
Hafizuddin-Abd-Rahman-Dev-Upstream <hafizuddin.abdrahm@petronas.com>
2025-09-19 11:42:39 +0800

    Update b) ABEX AI.py

--- pages/b) ABEX AI.py
+++ pages/b) ABEX AI.py
@@ -171,7 +171,7 @@ def main():
             "Upload CSV files (max 200MB)", type="csv", accept_multiple_files=True
         )
         # Add this block below the file uploader
-        st.sidebar.markdown("   📁 Or access data from Enterprise SharePoint")
+        st.sidebar.markdown("#📁 Or access data from Enterprise SharePoint")
         data_link = "https://petronas.sharepoint.com/sites/ecm_ups_coe/confidential/DFE%20Cost%20Engineering/Forms/AllItems.aspx?id=%2Fsites%2Fecm%5Fups%5Fcoe%2Fconfidential%2FDFE%20Cost%20Engineering%2F2%2ETemplate%20Tools%2FCost%20Predictor%2FDatabase&viewid=25092e6d%2D373d%2D41fe%2D8f6f%2D486cd8cdd5b8"  # <-- CHANGE THIS TO YOUR ACTUAL LINK
         st.sidebar.markdown(
             f'<a href="{data_link}" target="_blank"><button style="background-color:#0099ff;color:white;padding:8px 16px;border:none;border-radius:4px;">Open Data Storage</button></a>',
